dd60fe2ba57e133984111cb1878d093b6c11b9a7
Jstrs <84945307+jiajie0905@users.noreply.github.com>
2022-09-03 17:12:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjVglIuCXo34CsraXWEio1i3P7sZJ7IQEES93lDsgc4yOU",
+  CALLBACK_TEMPLATE_ID: "D3l2XrHwbRxA2wIjPEcBBT5kIHbwQxa-DoXe2XhxVl4",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
